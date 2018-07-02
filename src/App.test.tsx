1b983f7fd730cc26b202cb1d333a00e81d03aaa9
libo <jianglibo@gmail.com>
2018-07-02 21:32:08 +0800

    add enzyme dependency.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,3 +1,4 @@
+import * as enzyme from 'enzyme';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
@@ -6,4 +7,9 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the correct text when no enthusiasm level is given', () => {
+  const app = enzyme.shallow(<App />);
+  expect(app.find(".App-intro").text()).toContain('src/App.tsx');
 });
